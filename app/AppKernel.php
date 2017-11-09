b19c3ce5e35c98d50c4c86376236543d844859fa
IvanII <ivanoviaa92@gmail.com>
2017-11-10 00:44:18 +0600

    Added new bundle and doctrine-migrations

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            //app Bundles
             new AppBundle\AppBundle(),
             new BlogBundle\BlogBundle(),
         ];
